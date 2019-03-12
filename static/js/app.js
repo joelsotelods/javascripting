3170b63ecab7623f7fc96c363e684770980d47ff
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-03-11 21:28:08 -0600

    Added the functionality to read from the form when the button is clicked

--- static/js/app.js
+++ static/js/app.js
@@ -4,15 +4,49 @@
 //imported.src = '/path/to/imported/script';
 //document.head.appendChild(imported);
 
-var tableData = data;
+var ufo_data = data;
 
-console.log(tableData[0]);
+
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  // Set the span tag in the h1 element to the text
+  // that was entered in the form
+
+});
+
+
+
+// Create a custom filtering function
+function selectUFO(ufo_sight) {
+    return ufo_sight.datetime == "1/13/2010";
+}
+
+// filter() uses the custom function as its argument
+var selected_ufo_data = ufo_data.filter(selectUFO);
+
+// Test
+console.log(selected_ufo_data);
+
+
 
 var tbody = d3.select("tbody");
 
-
 // Iterate through each student/grade pair
-tableData.forEach(({datetime, city, state , country, shape, durationMinutes, comments}) => {
+selected_ufo_data.forEach(({datetime, city, state , country, shape, durationMinutes, comments}) => {
 
   // Append one table row per student/grade
   var row = tbody.append("tr");
