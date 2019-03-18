ccc1941d66935c152ce53321e302e8885110330b
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-03-18 15:33:36 -0600

    Added filter functionality to all fields

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,6 @@
 // from data.js
 var ufo_data = data;
 
-
 /// init the page... show all
 
 fill_the_table(ufo_data);
@@ -11,20 +10,22 @@ get_filter_values(ufo_data);
 
 var clear_filter = d3.select("#clear-btn");
 
-var date_filter = d3.select("#datetime_x");
+var date_filter = d3.select("#date_sel");
+var country_filter = d3.select("#country_sel");
+var state_filter = d3.select("#state_sel");
+var city_filter = d3.select("#city_sel");
+var shape_filter = d3.select("#shape_sel");
 
-
+var current_working_data = ufo_data;
 
 
 date_filter.on("change", function() {
-    d3.event.preventDefault();
-    console.log("hola")
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime_x");
+    var inputElement = d3.select("#date_sel");
 
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
@@ -32,9 +33,9 @@ date_filter.on("change", function() {
     console.log(inputValue);
 
     if (inputValue == "All Data") {
-        console.log("ja");
 
         fill_the_table(ufo_data);
+        get_filter_values(ufo_data);
 
     } else {
         // Create a custom filtering function
@@ -49,8 +50,153 @@ date_filter.on("change", function() {
         console.log(selected_ufo_data);
 
         fill_the_table(selected_ufo_data);
+        //get_filter_values(selected_ufo_data);
     }
 
+});
+
+
+country_filter.on("change", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#country_sel");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    if (inputValue == "All Data") {
+
+        fill_the_table(ufo_data);
+        get_filter_values(ufo_data);
+
+    } else {
+        // Create a custom filtering function
+        function selectUFO(ufo_sight) {
+            return ufo_sight.country == inputValue;
+        }
+
+        // filter() uses the custom function as its argument
+        var selected_ufo_data = ufo_data.filter(selectUFO);
+
+        // Test
+        console.log(selected_ufo_data);
+
+        fill_the_table(selected_ufo_data);
+        //get_filter_values(selected_ufo_data);
+    }
+
+});
+
+state_filter.on("change", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#state_sel");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    if (inputValue == "All Data") {
+
+        fill_the_table(ufo_data);
+        get_filter_values(ufo_data);
+
+    } else {
+        // Create a custom filtering function
+        function selectUFO(ufo_sight) {
+            return ufo_sight.state == inputValue;
+        }
+
+        // filter() uses the custom function as its argument
+        var selected_ufo_data = ufo_data.filter(selectUFO);
+
+        // Test
+        console.log(selected_ufo_data);
+
+        fill_the_table(selected_ufo_data);
+        //get_filter_values(selected_ufo_data);
+    }
+
+});
+
+city_filter.on("change", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#city_sel");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    if (inputValue == "All Data") {
+
+        fill_the_table(ufo_data);
+        get_filter_values(ufo_data);
+
+    } else {
+        // Create a custom filtering function
+        function selectUFO(ufo_sight) {
+            return ufo_sight.city == inputValue;
+        }
+
+        // filter() uses the custom function as its argument
+        var selected_ufo_data = ufo_data.filter(selectUFO);
+
+        // Test
+        console.log(selected_ufo_data);
+
+        fill_the_table(selected_ufo_data);
+        //get_filter_values(selected_ufo_data);
+    }
+
+});
+
+shape_filter.on("change", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#shape_sel");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    if (inputValue == "All Data") {
+
+        fill_the_table(ufo_data);
+        get_filter_values(ufo_data);
+
+    } else {
+        // Create a custom filtering function
+        function selectUFO(ufo_sight) {
+            return ufo_sight.shape == inputValue;
+        }
+
+        // filter() uses the custom function as its argument
+        var selected_ufo_data = ufo_data.filter(selectUFO);
+
+        // Test
+        console.log(selected_ufo_data);
+
+        fill_the_table(selected_ufo_data);
+        //get_filter_values(selected_ufo_data);
+    }
 
 });
 
@@ -60,6 +206,7 @@ clear_filter.on("click", function() {
     d3.event.preventDefault();
 
     fill_the_table(ufo_data);
+    get_filter_values(ufo_data);
 
 });
 
@@ -67,7 +214,6 @@ clear_filter.on("click", function() {
  
 
 function fill_the_table(data_to_show){
-
 
     var tbody = d3.select("tbody");
 
@@ -93,7 +239,7 @@ function fill_the_table(data_to_show){
 
 function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
-}
+};
 
 
 
@@ -104,10 +250,16 @@ function get_filter_values(data_to_show){
     dates.push("All Data");
 
     var countries = new Array();
-    var states = new Array();
-    var cities = new Array();
-    var shapes = new Array();
+    countries.push("All Data");
 
+    var states = new Array();
+    states.push("All Data");
+
+    var cities = new Array();
+    cities.push("All Data");
+    
+    var shapes = new Array();
+    shapes.push("All Data");
 
     // Iterate through each student/grade pair
     data_to_show.forEach(({datetime, city, state , country, shape, durationMinutes, comments}) => {
@@ -153,7 +305,7 @@ function get_filter_values(data_to_show){
 
     // Append filter array to selector..
 
-    var selector = d3.select("#datetime_x");
+    var selector = d3.select("#date_sel");
 
     selector.html("");
 
@@ -161,30 +313,28 @@ function get_filter_values(data_to_show){
 
 
 
-    var selector = d3.select("#sel1");
+    var selector = d3.select("#country_sel");
 
     selector.html("");
 
     country_u.forEach(myFunction);
 
   
-    var selector = d3.select("#sel2");
+    var selector = d3.select("#state_sel");
 
     selector.html("");
     state_u.forEach(myFunction);
 
-    var selector = d3.select("#sel3");
+    var selector = d3.select("#city_sel");
 
     selector.html("");
     cities_u.forEach(myFunction);
 
-    var selector = d3.select("#sel4");
+    var selector = d3.select("#shape_sel");
 
     selector.html("");
     shape_u.forEach(myFunction);
 
-
-    
 
 };
 
